7cc290db82de9f0496d08d724444289b1ec2835d
Debasis Kar <debasiskar007@gmail.com>
2018-05-07 14:25:50 +0530

    commit with login and 3 types of dashboard newlyy created components

---
+++ src/app/recruiterdashboard/recruiterdashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recruiterdashboard',
+  templateUrl: './recruiterdashboard.component.html',
+  styleUrls: ['./recruiterdashboard.component.css']
+})
+export class RecruiterdashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/salesrepdashboard/salesrepdashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-salesrepdashboard',
+  templateUrl: './salesrepdashboard.component.html',
+  styleUrls: ['./salesrepdashboard.component.css']
+})
+export class SalesrepdashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
