2ed8923904c30f2db2f9e8f9f5b5660c162a08d6
Debasis Kar <debasiskar007@gmail.com>
2018-05-07 14:24:07 +0530

    commit with login and 3 types of dashboard newlyy created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RecruiterdashboardComponent } from './recruiterdashboard/recruiterdashboard.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SalesrepdashboardComponent } from './salesrepdashboard/salesrepdashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RecruiterdashboardComponent,
+    DashboardComponent,
+    SalesrepdashboardComponent
   ],
   imports: [
     BrowserModule
