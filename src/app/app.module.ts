06cd0c03873a7f64350eab8e9158c43e1b9d9561
Debasis Kar <debasiskar007@gmail.com>
2018-05-08 15:21:25 +0530

    css n bootstrap integration !!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ReplaySubject} from 'rxjs/ReplaySubject';
+import {Subscription} from 'rxjs/Subscription';
 import {appRoutingProviders, routing} from './routes';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -7,6 +9,7 @@ import { RecruiterdashboardComponent } from './recruiterdashboard/recruiterdashb
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SalesrepdashboardComponent } from './salesrepdashboard/salesrepdashboard.component';
 import { RepsignupComponent } from './repsignup/repsignup.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,8 @@ import { RepsignupComponent } from './repsignup/repsignup.component';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    BsDropdownModule.forRoot()
   ],
   providers: [appRoutingProviders],
   bootstrap: [AppComponent]
