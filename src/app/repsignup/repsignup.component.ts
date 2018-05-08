e0ada2aa92ab14c6f4ceb7264f8541f584571375
Debasis Kar <debasiskar007@gmail.com>
2018-05-08 14:25:52 +0530

    repsignup page

---
+++ src/app/repsignup/repsignup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repsignup',
+  templateUrl: './repsignup.component.html',
+  styleUrls: ['./repsignup.component.css']
+})
+export class RepsignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
