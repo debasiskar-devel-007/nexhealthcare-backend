5a2c05456137aa283073ab5f28f3721b757b2c85
Debasis Kar <debasiskar007@gmail.com>
2018-05-08 19:25:35 +0530

    header , footer and recruiter signup !

---
+++ src/app/recruitersignup/recruitersignup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recruitersignup',
+  templateUrl: './recruitersignup.component.html',
+  styleUrls: ['./recruitersignup.component.css']
+})
+export class RecruitersignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
