acfa568fa0248aa37044d6ad45a29540d8aefab6
Debasis Kar <debasiskar007@gmail.com>
2018-05-11 17:05:03 +0530

    patient pages

---
+++ src/app/pateintdetail/pateintdetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pateintdetail',
+  templateUrl: './pateintdetail.component.html',
+  styleUrls: ['./pateintdetail.component.css']
+})
+export class PateintdetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
