94365d686dddefec9ee6e080b492fdb721a1e807
amitavainfluxiq <amitava.involutiontech@gmail.com>
2018-05-11 11:07:56 +0530

    html (amitava 11.05.2018)

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+    public termsmodal: boolean = false;
+    public policymodal: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+    callterms() {
+        this.termsmodal = true;
+    }
+    callpolicy() {
+        this.policymodal = true;
+    }
+    onHidden() {
+        this.termsmodal = false;
+        this.policymodal = false;
+    }
 }
