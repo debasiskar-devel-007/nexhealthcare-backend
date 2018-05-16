55f9461ff68b4aa33b93d835495a9916d6afaa6d
Debasis Kar <debasiskar007@gmail.com>
2018-05-15 20:58:30 -0800

    compensationmodifier page

---
+++ src/app/compensationmodifier/compensationmodifier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compensationmodifier',
+  templateUrl: './compensationmodifier.component.html',
+  styleUrls: ['./compensationmodifier.component.css']
+})
+export class CompensationmodifierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
