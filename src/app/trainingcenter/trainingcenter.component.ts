b39637f676dc487b58ebc42aa7ecfabee0e604cb
Debasis Kar <debasiskar007@gmail.com>
2018-05-31 12:01:56 +0530

    dev by ips on 30th

---
+++ src/app/trainingcenter/trainingcenter.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trainingcenter',
+  templateUrl: './trainingcenter.component.html',
+  styleUrls: ['./trainingcenter.component.css']
+})
+export class TrainingcenterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+
+
+
+}
