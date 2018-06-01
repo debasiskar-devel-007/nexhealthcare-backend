c1e68e46104bb15e0d744ae607668ba0098d3805
ipsitainfluxiq <ipsita.influxiq@gmail.com>
2018-06-01 14:29:25 +0530

    development by ipsita on 6/1/18

---
+++ src/app/sharelink/sharelink.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sharelink',
+  templateUrl: './sharelink.component.html',
+  styleUrls: ['./sharelink.component.css']
+})
+export class SharelinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
