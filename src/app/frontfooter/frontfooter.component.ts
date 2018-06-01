c9e224765e5dba4034f2961c8f37ef548457fd9d
ipsitainfluxiq <ipsita.influxiq@gmail.com>
2018-06-01 11:39:47 +0530

    development by ipsita on 6/1/18

--- src/app/frontfooter/frontfooter.component.ts
+++ src/app/frontfooter/frontfooter.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./frontfooter.component.css']
 })
 export class FrontfooterComponent implements OnInit {
-
+    public termsmodal: boolean = false;
+    public policymodal: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+    callterms() {
+        this.termsmodal = true;
+    }
+    callpolicy() {
+        this.policymodal = true;
+    }
+    onHidden() {
+        this.termsmodal = false;
+        this.policymodal = false;
+    }
 }
